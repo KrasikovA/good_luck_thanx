ab4bc3204cd83e97e19edc1dddcb1420d333c534
KrasikovA <krasikov.af@gmail.com>
2016-07-14 21:45:38 +0300

    regex and authentication added

--- main.rb
+++ main.rb
@@ -3,21 +3,42 @@ require 'telegram/bot'
 
 
 class Duty < ActiveRecord::Base
+	validates_uniqueness_of :username
 end
-p Duty.all
+
 TOKEN = ENV['GOOD_LUCK_THANX']
 time = Time.new
 time_values = time.to_a
-Telegram::Bot::Client.run(TOKEN) do |bot|
-  bot.listen do |message|
-	if time_values[2] > 20 and time_values[2] < 22 and !duty.greeted_evening
-      		bot.api.send_message(chat_id: message.chat.id, text: "Удачи")
-		duty.greeted_morning = false
-		duty.greeted_evening = true
-    	elsif time_values[2] > 8 and time_values[2] < 10 and !duty.greeted_morning
-      		bot.api.send_message(chat_id: message.chat.id, text: "Спасибо")
-		duty.greeted_morning = true
-		duty.greeted_evening = false
-    	end
+is_response = false
+Telegram::Bot::Client.run(TOKEN) do |bot| 
+ bot.listen do |message|
+	if is_response
+                if Duty.new(username: message.text).valid?
+			Duty.new(username: message.text)
+                        bot.api.send_message(chat_id: message.chat.id, text: "Дежурный #{message.text} успешно создан")
+                else
+                        bot.api.send_message(chat_id: message.chat.id, text: "Кажется пользователь #{message.text} уже существует")
+                end
+                is_response = false
+        end
+	if message.text == '/add_duty' and !is_response
+		bot.api.send_message(chat_id: message.chat.id, text: "Введите username дежурного") 
+		is_response = true
+	else
+		current_duty = Duty.find_by(username: message.from.username)
+		if current_duty
+			if time_values[2] > 20 and time_values[2] < 22 and !current_duty.evening_greeting and 
+			  message.text =~ /(на смене|начал|на дежурстве|заступил)/i
+      				bot.api.send_message(chat_id: message.chat.id, text: "Удачи")
+				current_duty.update(morning_greeting: false)
+				current_duty.update(evening_greeting: true)
+    			elsif time_values[2] > 8 and time_values[2] < 10 and !duty.morning_greeting and 
+			  message.text =~ /(закончил|окончил|завершил)/i
+      				bot.api.send_message(chat_id: message.chat.id, text: "Спасибо")
+				current_duty.update(morning_greeting: true)
+				current_duty.update(evening_greeting: false)
+    			end
+		end
+	end
   end
 end
