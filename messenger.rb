df42bd42f2b930a74f667ebb409d4311a11a6a08
KrasikovA <krasikov.af@gmail.com>
2016-08-03 02:03:55 +0300

    flickr images added

--- messenger.rb
+++ messenger.rb
@@ -1,14 +1,22 @@
+require 'flickraw'
 class Messenger 
     HELP = "/add_duty - adds new user to mailing list\n/say_good_luck - says good luck to everyone\n/say_thanx - says thank you to everyone"
+    PER_PAGE = 100
+    PAGES_ALL = 1000
     def initialize(bot, chat_id)
         @bot = bot
-        @chat_id = chat_id 
+        @chat_id = chat_id
+	FlickRaw.api_key=ENV["FLICKR_KEY"] 
+	FlickRaw.shared_secret=ENV["FLICKR_SECRET"]
     end
 
     def say(text)
         @bot.api.send_message(chat_id: @chat_id, text: text)
     end
-    def show(photo_path,photo_type)
-        @bot.api.send_photo(chat_id: @chat_id, photo: Faraday::UploadIO.new(photo_path, photo_type))	    
+    def show
+	page_random = Random.new().rand(1..PAGES_ALL)
+	on_page_random = Random.new().rand(0..PER_PAGE - 1)
+	image = flickr.groups.pools.getPhotos(group_id: '661812@N25',extras: 'url_c',per_page: PER_PAGE.to_s, page: page_random.to_s)[on_page_random]
+        @bot.api.send_message(chat_id: @chat_id, text: image.url_c )	    
     end
 end
